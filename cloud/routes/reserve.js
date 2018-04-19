1fca1dc2002883807d21355394c95d4d877145a8
bvthakur <bvthakur@ncsu.edu>
2018-04-19 16:01:25 -0400

    Database connectivity added

--- cloud/routes/reserve.js
+++ cloud/routes/reserve.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('mysql')
+var main = require('../app');
 /* POST user signup. */
+var con = mysql.createConnection({
+  host: "159.89.234.84",
+  user: "pavi",
+  password: "csc547",
+  database: "csc547caas"
+});
+
 router.post('/', function(req ,res) {
     console.log('This is reserve route');
     let data = req.body;
     console.log(data);
-    // Insert the data into database and spinup the container. 
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log('COnnected succesfully')
+    })
+    // Insert the data into database and spinup the container.
 
 })
 
