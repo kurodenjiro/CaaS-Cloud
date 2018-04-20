0833561bab2fa287e7ec285d70fcd8358e693eab
Pavithra Iyer <piyer3@ncsu.edu>
2018-04-20 17:14:27 -0400

    insert container details to DB

--- cloud/routes/reserve.js
+++ cloud/routes/reserve.js
@@ -27,6 +27,7 @@ router.post('/', function(req ,res) {
   let portmap = "";
   let conhash = "";
   let usedports = [];
+  let imid = 0;
 //1. image id,ports from DB
 
 
@@ -36,7 +37,7 @@ router.post('/', function(req ,res) {
 
   return new Promise(function(resolve, reject) {
       con.connect(function(err) {
-          let imquery = "SELECT import from images,image_ports WHERE images.imid = image_ports.imid AND images.tag = '"+ data.service+"'";
+          let imquery = "SELECT import,images.imid from images,image_ports WHERE images.imid = image_ports.imid AND images.tag = '"+ data.service+"'";
 
           con.query(imquery, function(err, rows, fields){
               if(err) reject(err);
@@ -48,6 +49,7 @@ router.post('/', function(req ,res) {
       console.log(rows);
       // console.log(r);
       imageport = rows;
+      imid = rows[0].imid;
       let comportquery = "select comport from computer_ports where comid=2 AND comport NOT IN ( select comport from container_ports where comid=2)";
       // console.log(rows);
       return new Promise(function(resolve, reject) {
@@ -79,14 +81,13 @@ router.post('/', function(req ,res) {
           ssh.connect({
             host: '159.89.234.84',
             username: 'root',
-            privateKey: '/home/bhushan/.ssh/id_rsa'
+            privateKey: '/home/pavi/.ssh/mydo_rsa'
           }).then( function() {
 
             let sshCmd = 'docker run -d -p '+ portmap +' localhost:5000/my-python:latest';
 
             ssh.execCommand(sshCmd, { cwd:'/root' }).then(function(result) {
               console.log(result.stdout);
-              //conhash = result.stdout;
               resolve(result.stdout)
             });
           });
@@ -94,8 +95,50 @@ router.post('/', function(req ,res) {
 
   },errHandler).then(function(result){
       console.log(result, "conhash");
-            //6. If it works properly -- insert first 12 character of hash in DB
+      //6. If it works properly -- insert first 12 character of hash in DB
+      conhash = result;
+      let containerquery = "INSERT INTO container(conid,conhash,comid,uid,imid,profile,res_start_time,res_end_time,creation_time,modified_time,state) VALUES(4,'"+result.substring(0,11)+"', 2, 1,"+imid+",1,'2018-01-01 03:00:00','2018-01-01 03:00:00', NOW(), '2018-01-01 03:00:00', 'available')";
+      // console.log(rows);
+      return new Promise(function(resolve, reject) {
+        con.query(containerquery, function(err, controws, fields){
+          if (err) reject(err);
+          //console.log(comrows, "hello");
+          // return comrows;
+          if(err) console.log(err);
+          
+          resolve();
+        })
+      })
 
+  },errHandler).then(function(){
+      let getconidquery = "SELECT conid from container ORDER BY conid desc LIMIT 1";
+      // console.log(rows);
+      return new Promise(function(resolve, reject) {
+        con.query(getconidquery, function(err, idrows, fields){
+          if (err) reject(err);
+          //console.log(comrows, "hello");
+          // return comrows;
+          //if(err) console.log(err);
+          console.log(idrows[0].conid);
+          resolve(idrows[0].conid);
+        })
+      })
+  },errHandler).then(function(result){
+      
+      return new Promise(function(resolve,reject){
+
+          for( var i=0; i < usedports.length; i++){
+              let portmappingquery = "INSERT INTO container_ports(conid, imid, import, comid, comport) VALUES("+result+","+imid+", "+usedports[i].import+","+2+","+usedports[i].comport+")";
+              con.query(portmappingquery, function(err,rows,fields){
+                  if(err) reject(err);
+                  resolve();
+              });
+          }
+
+        
+      })
+  },errHandler).then(function(){
+      console.log("Reservation completed");
   },errHandler);
 
 /*
