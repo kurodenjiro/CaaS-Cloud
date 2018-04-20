3d94d514fa162f33dcaa8ba041cbc3b17b734be7
bvthakur <bvthakur@ncsu.edu>
2018-04-20 17:13:57 -0400

    refactoring database

--- cloud/routes/signup.js
+++ cloud/routes/signup.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('../mysql');
 /* POST user signup. */
 router.post('/', function(req ,res) {
     console.log('This is good progress');
     let data = req.body;
     console.log(data);
+    sql = new mysql();
+    sql.con.connect(function(err) {
+      if(err) throw err;
+      console.log('THis is success')
+    })
     // put the data into the database
     // res.redirect to dashboard
     res.redirect('/dashboard');
